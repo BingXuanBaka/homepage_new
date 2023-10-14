0f2103c7bf47445a1f95cf915fa56cd50e04ec16
BingXuanOwO <hxc35459@outlook.com>
2023-10-15 03:00:10 +0800

    chore: add color styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#00A3E1",
+        background: "#292929",
+        container: "#3C3C3C",
+
+        'primary-text': "#FFFFFF",
+        'secondary-text': "#BFBFBF"
+      }
+    },
   },
   plugins: [],
 }
