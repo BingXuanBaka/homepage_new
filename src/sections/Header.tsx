280e0f9c6105f7a1086152cbb6dda61c8eaf0086
BingXuanOwO <hxc35459@outlook.com>
2023-10-16 08:20:37 +0800

    feat: add responsive for smaller screen to "Header" section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,7 +2,8 @@ function Header() {
   const description: string[] = ["Arch user", "UI 设计", "React", "死废物"]
 
   return (
-    <div class="h-[calc(100vh-6rem)] flex items-center justify-between">
+    <div class="h-[calc(100vh-6rem)] gap-10 flex items-center justify-between 
+      max-sm:items-start max-sm:flex-col-reverse max-sm:justify-center">
       <div>
         <h1 class="text-6xl text-primary-text leading-snug">
           好的<br />这里是
@@ -12,7 +13,7 @@ function Header() {
       </div>
 
       <div class="relative">
-        <img src="/avatar.png" alt="BingXuan's avatar" class="w-40 rounded-full" />
+        <img src="/avatar.png" alt="BingXuan's avatar" class="w-40 max-sm:w-36 rounded-full" />
         <img src="/avatar.png" aria-hidden="true"
           class="w-40 rounded-full absolute top-0 left-0 blur-2xl opacity-20" />
       </div>
