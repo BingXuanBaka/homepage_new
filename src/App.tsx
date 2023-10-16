a528498fecd02f6932703c3f41ad95902e74d393
BingXuanOwO <hxc35459@outlook.com>
2023-10-16 08:02:01 +0800

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "@fortawesome/fontawesome-free/css/all.css"
 import { TopBar } from "./components/TopBar"
 import { Header } from "./sections/Header"
 import { MainContent } from "./sections/MainContent"
+import { Footer } from "./sections/Footer"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div class="p-12 pt-0 max-sm:p-8 mx-auto max-w-7xl flex flex-col gap-12 text-primary-text">
         <Header />
         <MainContent />
+        <Footer />
       </div>
     </>
   )
---
+++ src/sections/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <span class="float-left font-mono">shhh... let’s not leak our hard work</span>
+      <span class="float-right font-mono">
+        by <a class="underline" href="https://bingxuan.org">冰轩</a>@2023
+      </span>
+    </footer>
+  )
+}
+
+export { Footer }
