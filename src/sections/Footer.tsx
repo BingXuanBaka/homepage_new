4a33c72b991355da58520153053ce137012af7c5
BingXuanOwO <hxc35459@outlook.com>
2023-10-16 20:04:52 +0800

    chore: change footer font size and color

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,8 +1,10 @@
 function Footer() {
   return (
-    <footer>
-      <span class="float-left font-mono">shhh... let’s not leak our hard work</span>
-      <span class="float-right font-mono">
+    <footer class="text-xs text-secondary-text font-mono">
+      <span class="float-left">
+        shhh... let’s not leak our hard work
+      </span>
+      <span class="float-right">
         by <a class="underline" href="https://bingxuan.org">冰轩</a>@2023
       </span>
     </footer>
